47bc8ca0251ef8a8a4c6d2024ba999521c32b0d8
Aliaksei Levashenia <mrfreeze987@gmail.com>
2023-12-19 19:20:58 +0300

    unused import

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Box, Button, Typography } from '@mui/material';
 
 export const NotFound = () => {
-    const navigate = useNavigate()
     return (
         <Box
             sx={{
